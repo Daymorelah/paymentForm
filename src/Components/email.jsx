8632e68349f98b834320b0dd422ecb7ba113b0d0
Ademola Hussain <demola.hussainin@gmail.com>
2020-06-17 08:30:38 +0100

    Validate email field

---
+++ src/Components/email.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import FormInput from './formInput';
+
+const Email = ({ setIsValidEntry, }) => {
+  const [inputValue, setInputValue] = useState('');
+  const validateInputField = () => {
+    const currentInputValue = inputValue.trim();
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(currentInputValue)) {
+      return setIsValidEntry(true);
+    }
+    return setIsValidEntry(false)
+  }
+
+  return (
+    <FormInput 
+      type="text"
+      id="email"
+      placeHolder="Email"
+      value={inputValue}
+      onChange={event => setInputValue(event.target.value)}
+      onBlur={() => validateInputField()}
+    />
+  );
+};
+
+Email.propTypes = {
+  setIsValidEntry: PropTypes.func.isRequired,
+};
+
+export default Email;
