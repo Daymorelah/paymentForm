5e895e5ea739b404431c5569ff1184e4d00177a7
Ademola Hussain <demola.hussainin@gmail.com>
2020-06-18 07:23:24 +0100

    Validate phone number field

--- src/Components/phoneNumber.jsx
+++ src/Components/phoneNumber.jsx
@@ -4,7 +4,24 @@ import FormInput from './formInput';
 
 const PhoneNumber = ({ setIsValidEntry, }) => {
   const [inputValue, setInputValue] = useState('');
+  const currentInputValue = inputValue.trim();
   const validateInputField = () => {
+    if(/[\s\D]/.test(currentInputValue)) {
+      return setIsValidEntry(false);
+    }
+    if(currentInputValue.length !== 11) {
+      return setIsValidEntry(false);
+    }
+    if(currentInputValue[0] !== '0') {
+      return setIsValidEntry(false);
+    }
+    if((currentInputValue[1] < 7) || (currentInputValue[1] > 9) ){
+      return setIsValidEntry(false);
+    }
+    if(currentInputValue[2] > 1) {
+      return setIsValidEntry(false);
+    }
+    console.log('logged outside');
     return setIsValidEntry(true);
   }
   return (
