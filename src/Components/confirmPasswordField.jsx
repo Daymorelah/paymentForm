e1dd6939f18332dd6a57e77fb82975d6a1c78ea5
Ademola Hussain <demola.hussainin@gmail.com>
2020-06-18 09:47:25 +0100

    Validate confirm password field

--- src/Components/confirmPasswordField.jsx
+++ src/Components/confirmPasswordField.jsx
@@ -2,26 +2,20 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import FormInput from './formInput';
 
-const PasswordField = ({ setIsValidEntry, }) => {
+const ConfirmPassword = ({ setIsValidEntry, initialPassword, }) => {
   const [inputValue, setInputValue] = useState('');
   const currentInputValue = inputValue.trim();
   const validateInputField = () => {
-    if( !(/[A-Z]+[0-9]+/.test(currentInputValue)) ){
+    if(currentInputValue !== initialPassword) {
       return setIsValidEntry(false);
     }
-    if( !(/[\W_]/.test(currentInputValue)) ){
-      return setIsValidEntry(false);
-    }
-    if(currentInputValue.length < 6) {
-      return setIsValidEntry(false);
-    }
-    return setIsValidEntry(true, currentInputValue);
+    return setIsValidEntry(true);
   }
   return (
     <FormInput
       type="password"
-      id="password"
-      placeHolder="Password"
+      id="confirm_password"
+      placeHolder="Confirm Password"
       value={inputValue}
       onChange={event => setInputValue(event.target.value)}
       onBlur={() => validateInputField()}
@@ -29,8 +23,10 @@ const PasswordField = ({ setIsValidEntry, }) => {
   );
 }
 
-PasswordField.propTypes = {
+ConfirmPassword.propTypes = {
   setIsValidEntry: PropTypes.func.isRequired,
+  initialPassword: PropTypes.string.isRequired,
 };
 
-export default PasswordField;
+
+export default ConfirmPassword;
