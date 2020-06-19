3c8ff77b4053d04fa60ca66239f77e298897de6b
Ademola Hussain <demola.hussainin@gmail.com>
2020-06-19 07:04:42 +0100

    Navigate user todashboard page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import PaymentForm from './PaymentForm';
+import Dashboard from './dashboard';
 const App = () => (
   <div id='app-container'>
-    <PaymentForm />
+    <Switch>
+      <Route exact path="/" component={PaymentForm} />
+      <Route exact path="/dashboard" component={Dashboard} />
+    </Switch>
   </div>
 );
 
